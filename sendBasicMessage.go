2d5b5a28b86ff1445becab52fec0554c97c0375a
Gerhard Molin <gerhard@minimumbadass.com>
2021-10-16 11:40:01 +0300

    basic bot works. had to install godotenv package to access .env variables

--- sendBasicMessage.go
+++ sendBasicMessage.go
@@ -6,14 +6,22 @@ import (
 	"os" // accessing environment variables
 
 	"github.com/slack-go/slack" //github.com slack library
+
+	"github.com/joho/godotenv"
 )
 
 func main() {
+	err := godotenv.Load()
+
+	if err != nil {
+		fmt.Printf("Error losaind .env file")
+	}
+
 	api := slack.New(os.Getenv("SLACK_OAUTH_TOKEN"))
 
 	channelID, timestamp, err := api.PostMessage(
 		"C02J0BZHMCK",
-		slack.MsgOptionText("Hello this is Hetki's bot speaking!", false),
+		slack.MsgOptionText("Hello fellow Go enthusiasts. This is Hetk's bot speaking!", false),
 	)
 
 	if err != nil {
@@ -21,5 +29,5 @@ func main() {
 		return
 	}
 
-	fmt.Printf("Message sent successfully to channel %s at %s", channelID, timestamp)
+	fmt.Printf("Message sent successfully to channel %s at %s\n", channelID, timestamp)
 }
