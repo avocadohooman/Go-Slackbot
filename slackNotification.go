fbe82cbe0bdfcb0dd1eacd826da621625afebbcf
Gerhard Molin <gerhard@minimumbadass.com>
2021-10-17 18:25:48 +0300

    first slackbot with slack UI framework works

--- slackNotification.go
+++ slackNotification.go
@@ -20,6 +20,10 @@ func main() {
 	args := os.Args[1:]
 	fmt.Println(args)
 
+	if len(args) != 4 {
+		fmt.Printf("Usage: jenkinsURL buildResult buildNumber jobName \n")
+		return 
+	}
 	api := slack.New(os.Getenv("SLACK_OAUTH_TOKEN"))
 	preText := "*Hello! Your GitHub Actions build has finished*"
 	jenkinsURL := "*Build URL:*" + args[0]
@@ -27,7 +31,7 @@ func main() {
 	buildNumber := "*" + args[2] + "*"
 	jobName := "*" + args[3] + "*"
 
-	if buildResult == "*SUCESS*" {
+	if buildResult == "*SUCCESS*" {
 		buildResult = buildResult + " :white_check_mark:"
 	} else {
 		buildResult = buildResult + " :x:"
@@ -37,4 +41,23 @@ func main() {
 	jenkinsBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + jenkinsURL
 	preTextField := slack.NewTextBlockObject("mrkdwn", preText + "\n\n", false, false)
 	jenkinsBuildDetailsField := slack.NewTextBlockObject("mrkdwn", jenkinsBuildDetails + "\n\n", false, false)
+
+	jenkinsBuildDetailsSection := slack.NewSectionBlock(jenkinsBuildDetailsField, nil, nil)
+	preTextSection := slack.NewSectionBlock(preTextField, nil, nil)
+
+	msg := slack.MsgOptionBlocks(
+		preTextSection,
+		dividerSection1,
+		jenkinsBuildDetailsSection,
+	)
+
+	_, _, _, err = api.SendMessage(
+		"C02J0BZHMCK",
+		msg,
+	)
+
+	if err != nil {
+		fmt.Printf("Error: %s\n", err)
+		return 
+	}
 }
