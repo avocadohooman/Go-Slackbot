1d44d0a3696924383bc965c90c3f71a2e6f599ce
Gerhard Molin <gerhard@minimumbadass.com>
2021-10-17 20:08:41 +0300

    jenkins => github

--- slackNotification.go
+++ slackNotification.go
@@ -21,13 +21,13 @@ func main() {
 	fmt.Println(args)
 
 	if len(args) != 4 {
-		fmt.Printf("Usage: jenkinsURL buildResult buildNumber jobName \n")
+		fmt.Printf("Usage: githubURL buildResult buildNumber jobName \n")
 		return 
 	}
 	
 	api := slack.New(os.Getenv("SLACK_OAUTH_TOKEN"))
 	preText := "*Hello! Your GitHub Actions build has finished*"
-	jenkinsURL := "*Build URL:*" + args[0]
+	githubURL := "*Build URL:*" + args[0]
 	buildResult := "*" + args[1] + "*"
 	buildNumber := "*" + args[2] + "*"
 	jobName := "*" + args[3] + "*"
@@ -39,21 +39,21 @@ func main() {
 	}
 
 	dividerSection1 := slack.NewDividerBlock()
-	jenkinsBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + jenkinsURL
+	githubBuildDetails := jobName + " #" + buildNumber + " - " + buildResult + "\n" + githubURL
 	preTextField := slack.NewTextBlockObject("mrkdwn", preText + "\n\n", false, false)
-	jenkinsBuildDetailsField := slack.NewTextBlockObject("mrkdwn", jenkinsBuildDetails + "\n\n", false, false)
+	githubBuildDetailsField := slack.NewTextBlockObject("mrkdwn", githubBuildDetails + "\n\n", false, false)
 
-	jenkinsBuildDetailsSection := slack.NewSectionBlock(jenkinsBuildDetailsField, nil, nil)
+	githubBuildDetailsSection := slack.NewSectionBlock(githubBuildDetailsField, nil, nil)
 	preTextSection := slack.NewSectionBlock(preTextField, nil, nil)
 
 	msg := slack.MsgOptionBlocks(
 		preTextSection,
 		dividerSection1,
-		jenkinsBuildDetailsSection,
+		githubBuildDetailsSection,
 	)
 
 	_, _, _, err = api.SendMessage(
-		"C02J0BZHMCK",
+		"C02J0BZHMC",
 		msg,
 	)
 
